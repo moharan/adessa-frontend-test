a6552a477c749d94b5e201d656f553b304ae0edf
RangiBecerra <rangibecerra@gmail.com>
2017-12-01 01:40:57 -0300

    product list style

--- static/js/apps/App.js
+++ static/js/apps/App.js
@@ -5,38 +5,66 @@ import { Grid, Row, Col, Button } from 'react-bootstrap'
 
 class App extends Component {
     render() {
-    const element = {
+    const elementCol = {
       borderRadius: 5,
-      padding: 5,
-      margin: 5,
+      paddingTop: 30,
       width: 100,
       height: 100,
-      backgroundColor: '#000000',
+      textAlign: 'center',
+      backgroundColor: '#666666',
+    }
+    const elementButton = {
+      marginTop: 10,
+    }
+    const backgroundGrid = {
+      backgroundColor: 'silver',
+      padding: 10,
+    }
+    const contentDiv = {
+      textAlign: 'center',
+      fontSize: 10,
+      marginTop: 5,
     }
 
     return (
       <Grid>
-          <Row>
-            <Col xs={10} md={10} xsOffset={2} mdOffset={2}>
+          <Row style={backgroundGrid}>
+            <Col xs={10} md={10} xsOffset={1} mdOffset={1}>
               <Col xs={2} md={2}>
-               <div style={element}>Contenido</div>
-                <Button>Default</Button>
+                <div style={elementCol}>Content</div>
+                <div style={contentDiv}>Product</div>
+                <div style={contentDiv}>$$$</div>
+                <Button style={elementButton} block>Add</Button>
               </Col>
               <Col xs={2} md={2}>
-              <div style={element}>Contenido</div>
-                <Button>Default</Button>
+                <div style={elementCol}>Content</div>
+                <div style={contentDiv}>Product</div>
+                <div style={contentDiv}>$$$</div>
+                <Button style={elementButton} block>Add</Button>
               </Col>
               <Col xs={2} md={2}>
-              <div style={element}>Contenido</div>
-                <Button>Default</Button>
+                <div style={elementCol}>Content</div>
+                <div style={contentDiv}>Product</div>
+                <div style={contentDiv}>$$$</div>
+                <Button style={elementButton} block>Add</Button>
               </Col>
               <Col xs={2} md={2}>
-              <div style={element}>Contenido</div>
-                <Button>Default</Button>
+                <div style={elementCol}>Content</div>
+                <div style={contentDiv}>Product</div>
+                <div style={contentDiv}>$$$</div>
+                <Button style={elementButton} block>Add</Button>
               </Col>
               <Col xs={2} md={2}>
-              <div style={element}>Contenido</div>
-                <Button>Default</Button>
+                <div style={elementCol}>Content</div>
+                <div style={contentDiv}>Product</div>
+                <div style={contentDiv}>$$$</div>
+                <Button style={elementButton} block>Add</Button>
+              </Col>
+              <Col xs={2} md={2}>
+                <div style={elementCol}>Content</div>
+                <div style={contentDiv}>Product</div>
+                <div style={contentDiv}>$$$</div>
+                <Button style={elementButton} block>Add</Button>
               </Col>
             </Col>
           </Row>
